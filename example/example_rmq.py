f32ce792dd4360b9690d8540883b7f3948d88988
miclon <jcnd@163.com>
2023-03-29 21:12:18 +0800

    🧪 test: more unit testing

--- example/example_rmq.py
+++ example/example_rmq.py
@@ -5,7 +5,7 @@ step.set_debugging()
 
 # 配置RabbitMQ队列及连接信息
 rmq_broker = RabbitMQBroker(
-    "test",
+    "test2",
     {
         "username": "admin",
         "password": "admin",
@@ -14,14 +14,16 @@ rmq_broker = RabbitMQBroker(
 
 # 模拟一个内存队列
 todo_broker = MemoryBroker()
-todo_broker.send("1")
 
 
-@step(from_broker=todo_broker, to_broker=rmq_broker)
+# todo_broker.send("1")
+
+
+@step(from_broker=rmq_broker, to_broker=todo_broker)
 def build_todo_list(message):
     print("build_todo_list", message.body)
     # 返回的内容将发给RabbitMQ队列
-    return "build_todo_list"
+    return message
 
 
 if __name__ == '__main__':
--- tests/test_memory_broker.py
+++ tests/test_memory_broker.py
@@ -1,16 +1,49 @@
+import json
+
+import pytest
 from onestep.broker import MemoryBroker
 
+message = "message1"
 
-def test_memory_broker_send():
-    broker = MemoryBroker()
-    message = "test message"
+
+@pytest.fixture
+def broker():
+    return MemoryBroker()
+
+
+@pytest.fixture
+def consumer(broker):
     broker.send(message)
-    assert broker.queue.get() == message
+    return broker.consume()
 
 
-def test_memory_broker_consume():
-    broker = MemoryBroker()
-    message = "test message"
-    broker.send(message)
+def test_memory_broker_send(broker, consumer):
+    # broker.queue.get() 拿到的是原始消息字符串，一般不需要直接使用
+    reached_message = broker.queue.get()
+    assert json.loads(reached_message).get("body") == message
+
+
+def test_memory_broker_consume(broker, consumer):
+    # broker.consume() 拿到的是 Message 对象
     consumer = broker.consume()
-    assert next(consumer) == message
+    assert next(consumer).body == message
+
+
+def test_memory_broker_reject(broker, consumer):
+    msg = next(consumer)
+    broker.reject(msg)
+    assert next(consumer) is None
+
+
+def test_memory_broker_requeue_source(broker, consumer):
+    msg = next(consumer)
+    msg.body = "message2"
+    broker.requeue(msg, is_source=True)  # 原始消息重新入队
+    assert next(consumer).body == message
+
+
+def test_memory_broker_requeue_not_source(broker, consumer):
+    msg = next(consumer)
+    msg.body = "message2"
+    broker.requeue(msg, is_source=False)  # 修改后的消息重新入队
+    assert next(consumer).body == "message2"
