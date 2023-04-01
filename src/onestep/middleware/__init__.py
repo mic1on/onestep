5ccffd2eec99a86952a01c81755e5ff9f1602349
NowAnti <believel.y@qq.com>
2023-04-01 15:12:12 +0800

    🦄 refactor(middleware.config): base/redis/nacos

--- src/onestep/middleware/__init__.py
+++ src/onestep/middleware/__init__.py
@@ -1,4 +1,6 @@
-from .config import (
-    NacosConfigMiddleware, RedisConfigMiddleware, BaseConfigMiddleware
-)
 from .base import BaseMiddleware
+from .config import (
+    BaseConfigMiddleware, PublishConfigMixin, ConsumeConfigMixin,
+    NacosConfigMixin, NacosPublishConfigMiddleware, NacosConsumeConfigMiddleware,
+    RedisConfigMixin, RedisPublishConfigMiddleware, RedisConsumeConfigMiddleware
+)
--- src/onestep/middleware/base.py
+++ src/onestep/middleware/base.py
@@ -1,13 +1,17 @@
 class BaseMiddleware:
 
-    def before_send(self, *args, **kwargs):
+    def before_send(self, step, message, *args, **kwargs):
         """消息发送之前"""
+        pass
 
-    def after_send(self, *args, **kwargs):
+    def after_send(self, step, message, *args, **kwargs):
         """消息发送之后"""
+        pass
 
-    def before_receive(self, *args, **kwargs):
+    def before_receive(self, step, message, *args, **kwargs):
         """消息接收之前"""
+        pass
 
-    def after_receive(self, *args, **kwargs):
+    def after_receive(self, step, message, *args, **kwargs):
         """消息接收之后"""
+        pass
