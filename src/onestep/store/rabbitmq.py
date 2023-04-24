e92d1256475e2ae1dcfa07837971074a39dd33c0
NowAnti <believel.y@qq.com>
2023-04-24 14:12:33 +0800

    🎈 perf: `RabbitmqStore`-> connection/channel add return type prompt

--- src/onestep/store/rabbitmq.py
+++ src/onestep/store/rabbitmq.py
@@ -11,7 +11,7 @@ logger = logging.Logger(__name__)
 
 
 class RabbitmqStore:
-
+    
     def __init__(self, *, confirm_delivery=True, host=None, port=None, username=None, password=None,
                  **kwargs):
         """
@@ -32,7 +32,7 @@ class RabbitmqStore:
         if kwargs:
             self.parameters.update(kwargs)
         self.confirm_delivery = confirm_delivery
-
+    
     def _create_connection(self):
         attempts = 1
         while attempts <= MAX_CONNECTION_ATTEMPTS:
@@ -42,23 +42,23 @@ class RabbitmqStore:
                 attempts += 1
                 logger.warning("RabbitmqStore connection error: %s", exc)
         raise AMQPConnectionError("RabbitmqStore connection error, max attempts reached")
-
+    
     @property
-    def connection(self):
+    def connection(self) -> amqpstorm.Connection:
         connection = getattr(self.state, "connection", None)
         if connection is None or not connection.is_open:
             connection = self.state.connection = self._create_connection()
         return connection
-
+    
     @connection.deleter
     def connection(self):
         if _connection := getattr(self.state, "connection", None):
             _connection.close()
             del self.state.connection
             del self.state.channel
-
+    
     @property
-    def channel(self):
+    def channel(self) -> amqpstorm.Channel:
         connection = getattr(self.state, "connection", None)
         channel = getattr(self.state, "channel", None)
         if all([connection, channel]) and all([connection.is_open, channel.is_open]):
@@ -67,13 +67,13 @@ class RabbitmqStore:
         if self.confirm_delivery:
             channel.confirm_deliveries()
         return channel
-
+    
     def declare_queue(self, queue_name, arguments=None):
         """声明队列"""
         if arguments is None:
             arguments = {}
         return self.channel.queue.declare(queue_name, durable=True, arguments=arguments)
-
+    
     def send(self, queue_name, message, priority=None, **kwargs):
         """发送消息"""
         attempts = 1
@@ -89,16 +89,16 @@ class RabbitmqStore:
                 attempts += 1
                 if attempts > MAX_SEND_ATTEMPTS:
                     raise exc
-
+    
     def flush_queue(self, queue_name):
         """清空队列"""
         self.channel.queue.purge(queue_name)
-
+    
     def get_message_counts(self, queue_name: str) -> int:
         """获取消息数量"""
         queue_response = self.declare_queue(queue_name)
         return queue_response.get("message_count", 0)
-
+    
     def start_consuming(self, queue_name, callback, prefetch=1, **kwargs):
         """开始消费"""
         while True:
