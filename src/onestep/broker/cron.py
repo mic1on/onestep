13469a78aa119aa46ff871190ea5171332cf04dd
miclon <jcnd@163.com>
2023-08-13 00:59:38 -0700

    ✨ feat: 增加BaseLocalBroker中Queue的maxsize参数 #16

--- src/onestep/broker/cron.py
+++ src/onestep/broker/cron.py
@@ -1,12 +1,15 @@
 """
 使用CRON表达式触发任务执行
 """
+import logging
 import threading
 from datetime import datetime
 
 from croniter import croniter
 
 from .base import BaseLocalBroker, BaseLocalConsumer
+
+logger = logging.getLogger(__name__)
 
 
 class CronBroker(BaseLocalBroker):
@@ -19,13 +22,10 @@ class CronBroker(BaseLocalBroker):
         self.kwargs = kwargs
         self._scheduler()
 
-    def _real_task(self):
-        self.queue.put_nowait(self.kwargs)
-
     def _scheduler(self):
         if self.next_fire_time <= datetime.now():
             self.next_fire_time = self.itr.get_next(datetime)
-            self._real_task()
+            self.publish(self.kwargs)
 
         threading.Timer(interval=1, function=self._scheduler).start()
 
